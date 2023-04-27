948a0e914354b8b26608474fbded07e6baeea746
tylerrussin <tylerrussin2@gmail.com>
2023-04-27 03:34:38 -0700

    Tune training params

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -18,7 +18,7 @@ class PersonalizedBase(Dataset):
                  size=None,
                  repeats=100,
                  interpolation="bicubic",
-                 flip_p=0.5,
+                 flip_p=0.0,
                  set="train",
                  placeholder_token="dog",
                  per_image_tokens=False,
